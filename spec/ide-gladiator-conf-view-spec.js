5d4dc85f0f94ef12d06b20b4dfc49fc9186b2cda
Roland Kister <rolandkister@gmail.com>
2019-03-22 17:38:44 +0100

    Initial commit

---
+++ spec/ide-gladiator-conf-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IdeGladiatorConfView from '../lib/ide-gladiator-conf-view';
+
+describe('IdeGladiatorConfView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
