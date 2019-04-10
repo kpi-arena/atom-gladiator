89b4178666bd9a5258d4e5fbb046e1b4e4d17d8f
Roland Kister <rolandkister@gmail.com>
2019-04-10 21:17:53 +0200

    Refactored document-builder

--- lib/connection.ts
+++ lib/connection.ts
@@ -3,10 +3,30 @@ import * as lsp from 'vscode-languageserver-protocol';
 import { SuperDocument } from './document-manager';
 
 export class SuperConnection extends LanguageClientConnection {
-  public didOpenTextDocument(params: lsp.DidOpenTextDocumentParams): void {
-    const test = new SuperDocument();
-    console.log(test.getSuperDoc(params));
+  private _docs: Map<string, SuperDocument> = new Map();
 
+  public didOpenTextDocument(params: lsp.DidOpenTextDocumentParams): void {
+    const doc = new SuperDocument(params);
+    doc.buildRequest(params);
     super.didOpenTextDocument(params);
+
+    // this._docs.set(doc.request.textDocument.uri, doc);
+  }
+
+  public onPublishDiagnostics(
+    callback: (params: lsp.PublishDiagnosticsParams) => void,
+  ): void {
+    const newCallback = (params: lsp.PublishDiagnosticsParams) => {
+      const doc = this._docs.get(params.uri);
+      let filteredParams = params;
+
+      if (doc) {
+        filteredParams = doc.filterAnswer(params);
+      }
+
+      callback(filteredParams);
+    };
+
+    super.onPublishDiagnostics(newCallback);
   }
 }
--- lib/document-manager.ts
+++ lib/document-manager.ts
@@ -3,18 +3,91 @@ import fs, { readFileSync } from 'fs';
 import * as path from 'path';
 import {
   DidOpenTextDocumentParams,
+  PublishDiagnosticsParams,
   TextDocument,
 } from 'vscode-languageserver-protocol';
 
 export class SuperDocument {
-  // private _roots: Map<string, ConfDocument> = new Map();
+  private readonly ROOT_REGEX = /^(\cI|\t|\x20)*#root:((\.|\\|\/|\w|-)+(\.yaml|\.yml))(\cI|\t|\x20)*/;
+  private readonly _languageID = 'yaml';
+  private readonly INCLUDE_REGEX = /^(\cI|\t|\x20)*#include:((\.|\\|\/|\w|-)+(\.yaml|\.yml))(\cI|\t|\x20)*/;
   private _newText: string = '';
   private _docs: Map<string, TextDocument> = new Map();
   private _linesRelation: ILinesRelation[] = [];
   private _rootDirectory: string = '';
-  private _rootRegex = /#root:\s*([^\n\r\s]*(\.yaml|\.yml))/i;
+  // private _request: DidOpenTextDocumentParams;
 
-  public getSuperDoc(
+  constructor(request: DidOpenTextDocumentParams) {
+    // this._request = this.createRequest(request);
+  }
+
+  public buildRequest(request: DidOpenTextDocumentParams) {
+    const editorDocs = this.getOpenYAMLDocuments();
+
+    const rootPath = this.getRootPath(
+      request.textDocument.text,
+      request.textDocument.uri,
+    );
+
+    console.log(rootPath);
+
+    const content = this.buildDoc('', rootPath, '', editorDocs);
+
+    const doc = TextDocument.create('', this._languageID, 0, content);
+    // @ts-ignore
+    const docOffsets: number[] = doc.getLineOffsets();
+
+    for (let index = 0; index < docOffsets.length; index++) {
+      const originalLine = doc.getText({
+        start: doc.positionAt(docOffsets[index]),
+        end: doc.positionAt(docOffsets[index + 1]),
+      });
+      console.log(`${docOffsets[index]}  - ${index} - ${originalLine}`);
+    }
+  }
+
+  // public get request(): DidOpenTextDocumentParams {
+  //   return this._request;
+  // }
+
+  public filterAnswer(
+    params: PublishDiagnosticsParams,
+  ): PublishDiagnosticsParams {
+    const doc = this._docs.get(Convert.uriToPath(params.uri));
+
+    if (!doc) {
+      return params;
+    }
+
+    const newParams: PublishDiagnosticsParams = {
+      uri: params.uri,
+      version: params.version,
+      diagnostics: [],
+    };
+
+    params.diagnostics.forEach(value => {
+      if (
+        this._linesRelation[value.range.start.line].originPath ===
+        Convert.uriToPath(doc.uri)
+      ) {
+        const newValue = value;
+
+        newValue.range.start.line = this._linesRelation[
+          value.range.start.line
+        ].originLine;
+
+        newValue.range.end.line = this._linesRelation[
+          value.range.end.line
+        ].originLine;
+
+        newParams.diagnostics.push(newValue);
+      }
+    });
+
+    return newParams;
+  }
+
+  private createRequest(
     request: DidOpenTextDocumentParams,
   ): DidOpenTextDocumentParams {
     this._docs = this.getDocuments(request);
@@ -39,7 +112,14 @@ export class SuperDocument {
 
     console.log(this._newText);
 
-    return request;
+    return {
+      textDocument: {
+        languageId: 'yaml',
+        version: rootDoc.version,
+        text: this._newText,
+        uri: request.textDocument.uri,
+      },
+    };
   }
 
   private recursiveTextInsert(doc: TextDocument, docPath: string) {
@@ -73,9 +153,157 @@ export class SuperDocument {
         this._linesRelation.push({
           originLine: docOffsets[index],
           originPath: path.join(this._rootDirectory, docPath),
+          intendationLength: 0,
         });
       }
     }
+  }
+
+  /**
+   * Revursively inserts TextDocuments given by `#include` comment in the docu-
+   * ment given by docPath. Return a `string`, which contains `newContent` con-
+   * cated with it's subdocuments.
+   *
+   * @param newContent - string to which the document, given by `docPath` is
+   * appended,
+   * @param docPath - path of the document to be parsed and appended,
+   * @param intendation - intedantion of the document that is appended to each
+   * inserted line,
+   * @param editorDocs - map of the documents open in the editor, where the key
+   * represents the path and value is a TextDocument created from the document.
+   */
+  private buildDoc(
+    newContent: string,
+    docPath: string,
+    intendation: string,
+    editorDocs: Map<string, TextDocument>,
+  ): string {
+    const docintendationLength = intendation.length;
+    let doc = editorDocs.get(docPath);
+
+    /* Checking if the document is open in the editor. In case it's not, get
+    the doc from the drive. */
+    if (!doc) {
+      doc = this.getDocumentFromDrive(docPath);
+
+      /* If the document could not be read, throw an error. */
+      if (!doc) {
+        throw new Error();
+      }
+    }
+
+    /* Using @ts-ignore to ignore the error, caused by accessing private method
+    to obtain line offsets od the document. */
+    // @ts-ignore
+    const docOffsets: number[] = doc.getLineOffsets();
+
+    for (let index = 0; index < docOffsets.length - 1; index++) {
+      const docLine = doc.getText({
+        start: doc.positionAt(docOffsets[index]),
+        end: doc.positionAt(docOffsets[index + 1]),
+      });
+
+      /* Appening the intendation of the document and the line from the docu-
+      ment to the newContent. */
+      newContent = newContent.concat(intendation, docLine);
+
+      this._linesRelation.push({
+        originLine: index,
+        originPath: docPath,
+        intendationLength: docintendationLength,
+      });
+
+      const includeMatch = docLine.match(this.INCLUDE_REGEX);
+
+      /* Checking, if the line contains #include statement recursively call
+      buildDoc(). */
+      if (includeMatch) {
+        const subDocPath = path.join(path.dirname(docPath), includeMatch[2]);
+
+        newContent = this.buildDoc(
+          newContent,
+          subDocPath,
+          intendation.concat(includeMatch[1] ? includeMatch[1] : ''),
+          editorDocs,
+        );
+      }
+    }
+
+    return newContent;
+  }
+
+  /**
+   * Creates a map from YAML documents in current workspace. Key value in the
+   * map represents path to the document. The value is a TextDocument created
+   * from document in buffer of the text editor. In case the document doesn't
+   * have a path it's ignored, since for including documents in other documents
+   * a path is required.
+   */
+  private getOpenYAMLDocuments(): Map<string, TextDocument> {
+    const result: Map<string, TextDocument> = new Map();
+
+    atom.workspace.getTextEditors().forEach(editor => {
+      const editorPath = editor.getBuffer().getPath();
+
+      /* Skipping file if it's not a YAML file saved on drive. */
+      if (!editorPath || !editorPath.match(/(\.yaml|\.yml)$/i)) {
+        return;
+      }
+
+      /* Pushing a TextDocument created from open YAML docoment. */
+      result.set(
+        editorPath,
+        TextDocument.create(
+          editor.getBuffer().getUri(),
+          this._languageID,
+          0,
+          editor.getBuffer().getText(),
+        ),
+      );
+    });
+
+    return result;
+  }
+
+  /**
+   * Reads the file given by `docPath` and creates TextDocument from it's
+   * content. Returns the created TextDocument or undefined, if the file
+   * could not been read.
+   * @param docPath - full path to the file containing YAML document.
+   */
+  private getDocumentFromDrive(docPath: string): TextDocument | undefined {
+    /* Creating a TextDocument from a YAML file located on the drive. If error
+    occurs while reading file, the error is caught and undefined is returned. */
+    try {
+      return TextDocument.create(
+        Convert.pathToUri(docPath),
+        this._languageID,
+        0,
+        readFileSync(docPath).toString(),
+      );
+    } catch (err) {
+      return undefined;
+    }
+  }
+
+  /**
+   * Checks if the `text` contains `#root` reference and returns the full path
+   * to the root document. In case it doesn't contain the reference, full path
+   * of the file given by uri is returned.
+   *
+   * @param text - content of a document,
+   * @param uri - uri of a document.
+   */
+  private getRootPath(text: string, uri: string): string {
+    const matchedComment = text.match(this.ROOT_REGEX);
+
+    /* Check if the first line contains '#root' comment. In case it doesn't
+    return path of this document. In case it does, return the path to the root. */
+    if (!matchedComment || !matchedComment[2]) {
+      return Convert.uriToPath(uri);
+    }
+
+    return path.join(path.dirname(Convert.uriToPath(uri)), matchedComment[2]);
   }
 
   private getDocuments(
@@ -200,19 +428,10 @@ export class SuperDocument {
 
     return documents;
   }
-
-  private getRootPath(text: string, uri: string): string {
-    const matchedComment = text.match(this._rootRegex);
-
-    if (!matchedComment || !matchedComment[1]) {
-      return Convert.uriToPath(uri);
-    }
-
-    return path.join(path.dirname(Convert.uriToPath(uri)), matchedComment[1]);
-  }
 }
 
 interface ILinesRelation {
   originLine: number;
   originPath: string;
+  intendationLength: number;
 }
